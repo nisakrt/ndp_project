b0570dd24042eb944325029421c979fbe84867b3
nisakrt <57156779+nisakrt@users.noreply.github.com>
2021-05-14 12:38:05 +0300

    new class

--- ndp_project/Program.cs
+++ ndp_project/Program.cs
@@ -23,7 +23,7 @@ namespace ndp_project
 
         }
     }
-    
- 
+
+
 }
 
---
+++ ndp_project/meyveler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ndp_project
+{
+    class meyveler
+    {
+        
+            private int vitaminA
+            {
+                get
+                {
+                    return vitaminA;
+                }
+                set { }
+            }
+            private int vitaminC
+            {
+                get
+                {
+                    return vitaminC;
+                }
+                set
+                { }
+            }
+            private int meyveGram
+            {
+                get
+                {
+                    return meyveGram;
+                }
+                set
+                { }
+            }
+
+
+
+    }
+}
